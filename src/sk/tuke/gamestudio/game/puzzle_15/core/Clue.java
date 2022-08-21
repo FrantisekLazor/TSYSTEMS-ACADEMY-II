1622e483144536fdaeb82121dd78e3f505ab2e1b
FrantisekLazor <111147012+FrantisekLazor@users.noreply.github.com>
2022-08-21 07:33:20 +0200

    Add files via upload

--- src/sk/tuke/gamestudio/game/puzzle_15/core/Clue.java
+++ src/sk/tuke/gamestudio/game/puzzle_15/core/Clue.java
@@ -1,13 +1,5 @@
 package sk.tuke.gamestudio.game.puzzle_15.core;
 
 public class Clue extends Tile {
-    private final int value;
 
-    public Clue(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
 }
--- src/sk/tuke/gamestudio/game/puzzle_15/core/Field.java
+++ src/sk/tuke/gamestudio/game/puzzle_15/core/Field.java
@@ -8,13 +8,13 @@ public class Field {
 
     private final int columnCount;
 
-    public Tile[][] tiles;
+    int[][] tiles;
 
     public Field(int rowCount, int columnCount) {
         this.rowCount = rowCount;
         this.columnCount = columnCount;
-        tiles = new Tile[rowCount][columnCount];
-        generate();
+        tiles = new int[rowCount][columnCount];
+
     }
 
 
@@ -26,61 +26,36 @@ public class Field {
         return columnCount;
     }
 
-    private void generate() {
-        fillWithNumbers();
-    }
-
-    private void fillWithNumbers() {
-        var random = new Random();
-//        int i = 1;
-//        for (var tile : tiles) {
-            var row = random.nextInt(rowCount);
-            var column = random.nextInt(columnCount);
-
-            }
-        }
-
-
     public void play() {
         printField();
-//        do {
-//            printField();
-//            processInput();
-//        } while (field.getState() == FieldState.PLAYING);
-//        printField();
-//        System.out.println(field.getState());
     }
 
     private void printField() {
-        for (var row = 0; row < rowCount; row++) {
-            for (var column = 0; column < columnCount; column++) {
-                if (tiles[row][column] == null) {
-                    tiles[row][column] = new Clue(countTiles(row, column));
+        var x = columnCount * rowCount;
+        for (var i = 0; i < tiles.length; i++) {
+            for (var j = 0; j < tiles[i].length; j++) {
+                tiles[i][j] = i + j;
+//                System.out.print(i + 1 + "\t");
+//                i++;
+            }
+
+//            System.out.println("\n");
+
+        }
+
+        for (int[] a : tiles) {
+            for (int i : a) {
+                        System.out.print(i + 1 + "\t");
+                        i++;
+                    }
+
+                    System.out.println("\n");
 
                 }
+
+
             }
         }
-    }
-    private int countTiles (int row, int column) {
-        var count = 0;
 
-        if (row > 0 && column > 0 && tiles[row][column] instanceof Tile)
-            count++;
-        if (row > 0 && tiles[row + 1][column] instanceof Tile)
-            count++;
-        if (row > 0 && column + 1 < columnCount && tiles[row - 1][column + 1] instanceof Mine)
-            count++;
-        if (column > 0 && tiles[row][column - 1] instanceof Mine)
-            count++;
-        if (column + 1 < columnCount && tiles[row][column + 1] instanceof Mine)
-            count++;
-        if (row + 1 < rowCount && column > 0 && tiles[row + 1][column - 1] instanceof Mine)
-            count++;
-        if (row + 1 < rowCount && tiles[row + 1][column] instanceof Mine)
-            count++;
-        if (row + 1 < rowCount && column + 1 < columnCount && tiles[row + 1][column + 1] instanceof Mine)
-            count++;
 
-        return count;
-    }
-}
+
